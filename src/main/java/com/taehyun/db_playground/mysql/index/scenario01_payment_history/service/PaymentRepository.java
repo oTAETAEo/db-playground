614df66e65f56ec5a8f2b6a7e496640f9e257d58
oTAETAEo <ggg7515@naver.com>
2026-07-09 05:19:33 +0900

    feat : JpaRepository 추가 및 시나리오 변경에 따른 조회 쿼리 기능 추가

---
+++ src/main/java/com/taehyun/db_playground/mysql/index/scenario01_payment_history/service/PaymentRepository.java
@@ -0,0 +1,26 @@
+package com.taehyun.db_playground.mysql.index.scenario01_payment_history.service;
+
+import com.taehyun.db_playground.mysql.index.scenario01_payment_history.domain.Payment;
+import com.taehyun.db_playground.mysql.index.scenario01_payment_history.domain.PaymentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    @Query("SELECT p FROM Payment p " +
+            "WHERE p.userId = :userId " +
+            "AND p.paymentType = :paymentType " +
+            "AND p.paymentStatus = 'SUCCESS' " +
+            "AND p.createdAt BETWEEN :startCreatedAt AND :endCreatedAt " +
+            "ORDER BY p.createdAt DESC")
+    List<Payment> findPaymentsByPeriod(
+            Long userId,
+            PaymentType paymentType,
+            LocalDateTime startCreatedAt,
+            LocalDateTime endCreatedAt
+    );
+
+}
