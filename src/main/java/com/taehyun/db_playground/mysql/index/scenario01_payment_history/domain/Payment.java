e538c8ac3aeb6dafffcbf1a55a8db2017f9d4325
oTAETAEo <ggg7515@naver.com>
2026-07-09 03:54:57 +0900

    feat: 인덱스 성능 테스트를 위한 Payment 엔티티 추가

---
+++ src/main/java/com/taehyun/db_playground/mysql/index/scenario01_payment_history/domain/Payment.java
@@ -0,0 +1,45 @@
+package com.taehyun.db_playground.mysql.index.scenario01_payment_history.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments_v1")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "amount", nullable = false)
+    private Long amount;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "payment_type", nullable = false)
+    private PaymentType paymentType;
+
+    @Column(name = "payment_status", nullable = false)
+    private String paymentStatus;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Payment(Long userId, Long amount, PaymentType paymentType, String paymentStatus, LocalDateTime createdAt) {
+        this.userId = userId;
+        this.amount = amount;
+        this.paymentType = paymentType;
+        this.paymentStatus = paymentStatus;
+        this.createdAt = createdAt;
+    }
+
+}
---
+++ src/main/java/com/taehyun/db_playground/mysql/index/scenario01_payment_history/domain/PaymentType.java
@@ -0,0 +1,5 @@
+package com.taehyun.db_playground.mysql.index.scenario01_payment_history.domain;
+
+public enum PaymentType {
+    CARD, TRANSFER, PAY
+}
