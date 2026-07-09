e2a96572ffae7944b588df7a756178009358fc3c
oTAETAEo <ggg7515@naver.com>
2026-07-10 00:02:35 +0900

    feat : 지갑 도메인 추가 및 잔고 감소 기능 추가

---
+++ src/main/java/com/taehyun/db_playground/mysql/lock/scenario01/domain/Wallet.java
@@ -0,0 +1,45 @@
+package com.taehyun.db_playground.mysql.lock.scenario01.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "wallets_v1")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long balance;
+
+    private Wallet(Long userId, Long balance) {
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public static Wallet createWallet(Long userId, Long balance){
+        return new Wallet(userId, balance);
+    }
+
+    public void deduct(Long amount) {
+
+        if (amount == null)
+            throw new IllegalArgumentException("null 일 수 없습니다");
+
+        if (amount == 0)
+            throw new IllegalArgumentException("차감할 금액이 0 일 수 없습니다");
+
+        if (this.balance < amount)
+            throw new IllegalArgumentException("잔고가 부족합니다");
+
+        this.balance -= amount;
+    }
+
+}
