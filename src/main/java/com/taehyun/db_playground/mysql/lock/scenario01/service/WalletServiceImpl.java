5959f40212d578ec22222f89432038ab9e5c9673
oTAETAEo <ggg7515@naver.com>
2026-07-10 00:11:44 +0900

    feat : 금액 차감 기능 구현

--- src/main/java/com/taehyun/db_playground/mysql/lock/scenario01/service/WalletServiceImpl.java
+++ src/main/java/com/taehyun/db_playground/mysql/lock/scenario01/service/WalletServiceImpl.java
@@ -1,16 +1,27 @@
 package com.taehyun.db_playground.mysql.lock.scenario01.service;
 
+import com.taehyun.db_playground.mysql.lock.scenario01.domain.Wallet;
+import com.taehyun.db_playground.mysql.lock.scenario01.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class WalletServiceImpl implements WalletService {
 
+    private final WalletRepository walletRepository;
+
     @Override
+    @Transactional
     public void decreaseBalance(Long userId, Long amount) {
 
+        Wallet wallet = walletRepository.findByUserId(userId)
+                .orElseThrow(NoSuchElementException::new);
+
+        wallet.deduct(amount);
     }
 
 }
