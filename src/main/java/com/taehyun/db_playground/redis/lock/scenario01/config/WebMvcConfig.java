b983f3f10fac0a527127494393724df4e1543461
oTAETAEo <ggg7515@naver.com>
2026-07-11 05:37:41 +0900

    feat : 인터셉터 등록

---
+++ src/main/java/com/taehyun/db_playground/redis/lock/scenario01/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.taehyun.db_playground.redis.lock.scenario01.config;
+
+import com.taehyun.db_playground.redis.lock.scenario01.interceptor.IdempotencyInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final IdempotencyInterceptor idempotencyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(idempotencyInterceptor)
+                .addPathPatterns("/order");
+    }
+
+}
