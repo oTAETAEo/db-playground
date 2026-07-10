c5efd689383da8650c2a8da9d77e2766e3ca6d92
oTAETAEo <ggg7515@naver.com>
2026-07-10 20:54:49 +0900

    feat : 사용자의 지갑 정보와 잔액을 나타내기 위한 클래스 추가

---
+++ src/main/java/com/taehyun/db_playground/redis/lock/scenario01/domain/Wallet.java
@@ -0,0 +1,44 @@
+package com.taehyun.db_playground.redis.lock.scenario01.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "wallet_v1")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long userId;
+
+    @Column(nullable = false)
+    private long balance;
+
+    private Wallet(Long userId, int balance) {
+        this.userId = userId;
+        this.balance = balance;
+    }
+
+    public static Wallet createWallet(Long userId, int balance){
+        return new Wallet(userId, balance);
+    }
+
+    public void deduct(int amount) {
+
+        if (amount == 0)
+            throw new IllegalArgumentException("차감할 금액이 0 일 수 없습니다");
+
+        if (this.balance < amount)
+            throw new IllegalArgumentException("잔고가 부족합니다");
+
+        this.balance -= amount;
+    }
+
+}
